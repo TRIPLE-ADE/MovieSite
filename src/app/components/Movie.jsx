78b2f1b6f8783ddaa5eba858bbe15ff6963bcee5
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-07-16 10:36:51 +0100

    feat: Update movies layout with next Image

--- src/app/components/Movie.jsx
+++ src/app/components/Movie.jsx
@@ -1,16 +1,22 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
 
 const Movie = ({ movie }) => {
-    const { id, title, poster_path, release_date } = movie
+  const { id, title, poster_path, release_date } = movie;
   return (
     <Link key={id} href={`/movies/${id}`}>
-        <div>
-            <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} alt={title}/>
-            <p>{title}</p>
-            <p>{release_date}</p>
-        </div>
+      <div>
+        <Image
+          src={`https://image.tmdb.org/t/p/w200${poster_path}`}
+          alt={title}
+          width={500}
+          height={500}
+        />
+        <p>{title}</p>
+        <p>{release_date}</p>
+      </div>
     </Link>
-)
-}
+  );
+};
 
-export default Movie
+export default Movie;
